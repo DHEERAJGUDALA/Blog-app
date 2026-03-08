563a4df1a0578a8cd00edd746e081488201f0b5b
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-03-08 17:00:58 +0530

    "Added Tag Repo"

---
+++ src/main/java/com/blog/blog_app/repository/TagRepository.java
@@ -0,0 +1,7 @@
+package com.blog.blog_app.repository;
+import com.blog.blog_app.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+}
